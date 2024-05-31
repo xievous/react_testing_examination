cf184d42dfe6df0cf162c0e42706fce755b4f7e4
Oscar <oscar.la02@outlook.com>
2024-05-31 15:31:42 +0200

    config commit

--- strajk-bowling/vite.config.js
+++ strajk-bowling/vite.config.js
@@ -1,18 +1,3 @@
-/*
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/mocks/setup.js",
-  },
-});
-*/
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { configDefaults } from "vitest/config";
